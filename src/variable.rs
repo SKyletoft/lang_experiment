41ee8031e9e14ee45283f3a997f0cbd9a41cf737
Samuel Kyletoft <skyletoft@gmail.com>
2020-11-18 15:41:32 +0100

    Fix for 0-length variable names and names starting with digits

--- src/variable.rs
+++ src/variable.rs
@@ -10,86 +10,88 @@ pub type CallStack = Vec<(HashMap<String, Variable>, usize)>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Variable {
-	Boolean(bool),
-	Number(f64),
-	Char(char),
-	List(VariableT, Vec<Variable>),
+    Boolean(bool),
+    Number(f64),
+    Char(char),
+    List(VariableT, Vec<Variable>),
 }
 
 impl fmt::Display for Variable {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match self {
-			Boolean(b) => write!(f, "{}", b),
-			Number(n) => write!(f, "{}", n),
-			Char(c) => write!(f, "{}", c),
-			List(_, l) => write!(f, "{:?}", l),
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Boolean(b) => write!(f, "{}", b),
+            Number(n) => write!(f, "{}", n),
+            Char(c) => write!(f, "{}", c),
+            List(_, l) => write!(f, "{:?}", l),
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum VariableT {
-	BooleanT,
-	NumberT,
-	CharT,
-	ListT(Box<VariableT>),
+    BooleanT,
+    NumberT,
+    CharT,
+    ListT(Box<VariableT>),
 }
 
 impl std::str::FromStr for VariableT {
-	type Err = SyntaxError;
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		let res = match s.trim() {
-			"f64" => NumberT,
-			"num" => NumberT,
-			"bool" => BooleanT,
-			"char" => CharT,
-			//"list" => ListT()
-			_ => return Err(SyntaxError {}),
-		};
-		Ok(res)
-	}
+    type Err = SyntaxError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let res = match s.trim() {
+            "f64" => NumberT,
+            "num" => NumberT,
+            "bool" => BooleanT,
+            "char" => CharT,
+            //"list" => ListT()
+            _ => return Err(SyntaxError {}),
+        };
+        Ok(res)
+    }
 }
 
 impl fmt::Display for VariableT {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		if let ListT(t) = self {
-			write!(f, "List of {}", t)
-		} else {
-			write!(
-				f,
-				"{}",
-				match self {
-					NumberT => "Number",
-					CharT => "Char",
-					BooleanT => "Boolean",
-					ListT(_) => unreachable!(),
-				}
-			)
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let ListT(t) = self {
+            write!(f, "List of {}", t)
+        } else {
+            write!(
+                f,
+                "{}",
+                match self {
+                    NumberT => "Number",
+                    CharT => "Char",
+                    BooleanT => "Boolean",
+                    ListT(_) => unreachable!(),
+                }
+            )
+        }
+    }
 }
 
 pub fn to_type(var: &Variable) -> VariableT {
-	match var {
-		Number(_) => NumberT,
-		Char(_) => CharT,
-		Boolean(_) => BooleanT,
-		List(t, _) => ListT(Box::new(t.clone())),
-	}
+    match var {
+        Number(_) => NumberT,
+        Char(_) => CharT,
+        Boolean(_) => BooleanT,
+        List(t, _) => ListT(Box::new(t.clone())),
+    }
 }
 
 pub fn evaluate_statement(words: &[&str], variables: &Variables) -> Result<Variable, CustomErr> {
-	let float = floats::evaluate_floats(words, &variables);
-	if float.is_ok() {
-		return float;
-	}
-	let b = bools::evaluate_bools(words, &variables);
-	if b.is_ok() {
-		return b;
-	}
-	Err(perr())
+    let float = floats::evaluate_floats(words, &variables);
+    if float.is_ok() {
+        return float;
+    }
+    let b = bools::evaluate_bools(words, &variables);
+    if b.is_ok() {
+        return b;
+    }
+    Err(perr())
 }
 
 pub fn is_ok(name: &str) -> bool {
-	KEYWORDS.contains(&name)
+    !KEYWORDS.contains(&name)
+        && name.len() >= 1
+        && name.as_bytes().get(0).map(|d| d.is_ascii_digit()) != Some(true)
 }
